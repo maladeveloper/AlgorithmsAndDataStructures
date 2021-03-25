6e48d8549510afb1e137ca29b402cf08fb91fadd
maladeveloper <58972378+maladeveloper@users.noreply.github.com>
2021-03-26 10:35:23 +1100

    Can sink the root value,
    Added code to sink the root value to the correct position.

--- Heaps/Heap.py
+++ Heaps/Heap.py
@@ -8,7 +8,7 @@ class MinHeap:
     
     def heapify(self, heap_arr):
         
-        for i in range(len(heap_arr)):
+        for i in range(len(heap_arr)): #Sink each element in array to its position
             
             self.sink_root(heap_arr)
     
@@ -18,6 +18,45 @@ class MinHeap:
         i = 0 
         
         while True:
+            
+            child_one_pos = (i*2 + 1)
+            
+            if child_one_pos < len(heap_arr): #Check if node has atleast one child
+                
+                min_child_pos = child_one_pos
+                
+                child_two_pos = (i*2 + 2)
+                
+                if  child_two_pos < len(heap_arr): #Check if it has a second child
+                    
+                    min_child_pos = min( [(heap_arr[child_one_pos], child_one_pos), (heap_arr[child_two_pos], child_two_pos)], key=lambda child_info: child_info[0])[1] #Get the minimum value child's position
+                    
+            else: #If it doesnt have a child then it has moved to correct position
+                
+                break
+            
+            if heap_arr[min_child_pos] < heap_arr[i]: #Swap if current node is smaller than the smallest child
+                
+                heap_arr[i], heap_arr[min_child_pos] = heap_arr[min_child_pos], heap_arr[i] #Swap nodes
+                
+                i = min_child_pos #Update the new index of the current node
+            
+            else: #This means current node is smaller than its child and can be kept in the same place
+                break
+    
+    def add_element(self, new_element):
+        
+        self.heap_arr.append(new_element)
+        
+        self.float_up(self.heap_arr)
+    
+    def float_up(self, heap_arr):
+        
+        i = len(heap_arr) - 1
+             
+                
+                
+            
             
             
             
