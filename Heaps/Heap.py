e58f8968ddb0f3ed7e236fd3479f1c647e31b915
maladeveloper <58972378+maladeveloper@users.noreply.github.com>
2021-03-26 09:19:52 +1100

    Update Heap.py
    1

--- Heaps/Heap.py
+++ Heaps/Heap.py
@@ -3,3 +3,24 @@ class MinHeap:
     def __init__(self, array=[]):
 
         self.heap_arr = array
+        
+        self.heapify(self.array)
+    
+    def heapify(self, heap_arr):
+        
+        for i in range(len(heap_arr)):
+            
+            self.sink_root(heap_arr)
+    
+    
+    def sink_root(heap_arr):
+        
+        i = 0 
+        
+        while True:
+            
+            
+            
+            
+            
+            
